706c05c5b1e43aa493b298cff56881c8152faa2f
James Asghar <9677449-james.asghar@users.noreply.gitlab.com>
2023-04-11 12:10:11 +0200

    removed luasnip

--- lua/maps.lua
+++ lua/maps.lua
@@ -179,16 +179,10 @@ require('nvim-treesitter.configs').setup {
   },
 }
 
-local luasnip = require("luasnip")
 local cmp = require("cmp")
 
 cmp.setup({
 
-    snippet = {
-        expand = function(args)
-          require('luasnip').lsp_expand(args.body)
-        end,
-    },
     window = {
         completion = cmp.config.window.bordered(),
         documentation = cmp.config.window.bordered(),
@@ -207,25 +201,11 @@ cmp.setup({
       select = false,
     }),
     ["<Tab>"] = cmp.mapping(function(fallback)
-      if cmp.visible() then
         cmp.select_next_item()
-      elseif luasnip.expand_or_jumpable() then
-        luasnip.expand_or_jump()
-      elseif has_words_before() then
-        cmp.complete()
-      else
-        fallback()
-      end
     end, { "i", "s" }),
 
     ["<S-Tab>"] = cmp.mapping(function(fallback)
-      if cmp.visible() then
         cmp.select_prev_item()
-      elseif luasnip.jumpable(-1) then
-        luasnip.jump(-1)
-      else
-        fallback()
-      end
     end, { "i", "s" }),
 
   },
@@ -233,7 +213,6 @@ cmp.setup({
   -- Installed sources
   sources = {
     { name = 'nvim_lsp' },
-    { name = 'luasnip' },
     { name = 'path' },
     { name = 'buffer' },
   },
--- lua/plugins.lua
+++ lua/plugins.lua
@@ -15,16 +15,11 @@ return require('packer').startup(function()
     -- LSP completion source for nvim-cmp
     use 'hrsh7th/cmp-nvim-lsp'
 
-    -- Snippet completion source for nvim-cmp
-    use 'saadparwaiz1/cmp_luasnip' 
 
     -- Other usefull completion sources
     use 'hrsh7th/cmp-path'
     use 'hrsh7th/cmp-buffer'
 
-    use "rafamadriz/friendly-snippets"
-    -- Snippet engine
-    use({"L3MON4D3/LuaSnip", tag = "v<CurrentMajor>.*"})
     -- Fuzzy finder
     -- Optional
     use 'nvim-lua/popup.nvim'
@@ -43,35 +38,7 @@ return require('packer').startup(function()
 
     use 'jakewvincent/texmagic.nvim'
 
-    use {
-        "Pocco81/auto-save.nvim",
-        config = function()
-            require("auto-save").setup {
-                enabled = false,
-            }
-        end,
-    }
 
-    use {
-    "nvim-neorg/neorg",
-    config = function()
-        require('neorg').setup {
-            load = {
-                ["core.defaults"] = {}, -- Loads default behaviour
-                ["core.norg.concealer"] = {}, -- Adds pretty icons to your documents
-                ["core.norg.dirman"] = { -- Manages Neorg workspaces
-                    config = {
-                        workspaces = {
-                            notes = "~/notes",
-                        },
-                    },
-                },
-            },
-        }
-    end,
-    run = ":Neorg sync-parsers",
-    requires = "nvim-lua/plenary.nvim",
-}
 
     if packer_bootstrap then
         require('packer').sync()
